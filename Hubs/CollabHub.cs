a2b6a26cadeda827afcadb8423f50b8370f52a4e
Bjoern Meyer <bjoern@textcontrol.com>
2021-06-08 10:28:11 +0200

    Add project files.

---
+++ Hubs/CollabHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace tx_viewer_collab.Hubs
+{
+    public class CollabHub : Hub
+    {
+        public async Task SetAnnotationSync(AnnotationSync annotationSync)
+        {
+            await Clients.All.SendAsync("ReceiveAnnotationSync", annotationSync);
+        }
+    }
+
+    public class AnnotationSync
+    {
+        public string User { get; set; }
+        public string AnnotationJson { get; set; }
+    }
+}
---
+++ wwwroot/js/collab.js
@@ -0,0 +1,53 @@
+﻿"use strict";
+
+//-------------------------------------------------------------------------------------------------
+// Global Fields
+//-------------------------------------------------------------------------------------------------
+var _updating;
+var _connection;
+
+// connect to signalr hub
+_connection = new signalR.HubConnectionBuilder().withUrl("/collabHub").build();
+_connection.start();
+
+_connection.on("ReceiveAnnotationSync", async function (annotationSync) {
+
+    // set flag to avoid infinite loops
+    _updating = true;
+
+    var currentJson = TXDocumentViewer.annotations.export();
+
+    if (currentJson != annotationSync.annotationJson)
+        TXDocumentViewer.annotations.load(annotationSync.annotationJson);
+
+    setTimeout(function () {
+        _updating = false;
+    }, 1000);
+    
+});
+
+window.addEventListener("documentViewerLoaded", function () {
+    TXDocumentViewer.addEventListener("annotationsChanged", updateAnnotations);
+});
+
+function updateAnnotations() {
+
+    if (_updating === true)
+        return;
+
+    var jsonAnnotations = TXDocumentViewer.annotations.export();
+
+    // create sync object
+    var collaborationSyncObject = {
+        User: "Test",
+        AnnotationJson: jsonAnnotations
+    };
+
+    // call signalr hub with sync object
+    _connection.invoke("SetAnnotationSync",
+        collaborationSyncObject).catch(function (err) {
+            return console.error(err.toString());
+        }
+    );
+
+}
